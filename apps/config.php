2d8c850ad2d931fbc6509f8592abe96f74768347
Shadownest <pascal.charreix@gmail.com>
2018-03-05 09:40:40 +0100

    maj config

--- apps/config.php
+++ apps/config.php
@@ -2,17 +2,9 @@
 
 /* Configuration SQL */
 
-<<<<<<< HEAD
 $dsn = 'mysql:dbname=restaurant;host=192.168.1.91';
 $db_host = "192.168.1.91";
 $db_user = "restaurant";
 $db_password = "ULaicha6ei";
 
-=======
-$dsn = 'mysql:dbname=restaurant;host=localhost';
-$db_user = "restaurant";
-$db_password = "ULaicha6ei";
-
-
->>>>>>> 8d7ea44b09aba9fdc0ffc73a21d1c868b61659ac
 ?>
