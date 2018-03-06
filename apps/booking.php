9b48d1768375ebfc06707d3532862101d3e5a18f
ingushsamad <ingushsamad@gmail.com>
2018-03-06 11:10:17 +0100

    maj samad

--- apps/booking.php
+++ apps/booking.php
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+<!--<<<<<<< HEAD-->
 <?php require('views/booking.phtml'); ?>
-=======
+<!--=======
 <?php
 require('views/booking.phtml');
 ?>
->>>>>>> ce7d60b1c284890ddb93e2cd6b57e6c4ba7fc3ef
+>>>>>>> ce7d60b1c284890ddb93e2cd6b57e6c4ba7fc3ef-->
